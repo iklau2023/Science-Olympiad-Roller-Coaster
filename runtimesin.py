ffaf30426b08184201439a86c55e661f621159c9
Space-Ranger1 <118828742+Space-Ranger1@users.noreply.github.com>
2023-01-26 14:22:03 -0600

    Add files via upload

---
+++ runtimesin.py
@@ -0,0 +1,8 @@
+import math
+d= float(input('What is the angle:'))
+sin= math.sin(math.radians(d))
+v= sin*18.2
+t=364.5/v
+t= round(t,2)
+print(f'Your run time is {t} seconds')
+#20-40 for comp
---
+++ score.py
@@ -0,0 +1,17 @@
+h= int(input('What is the height of the roller coaster in cm:'))
+hs= 2*(60-h)
+g= int(input('How long is your gap in cm:'))
+if g < 5 :
+    l=int(input('How tall is your loop in cm:'))
+    gs=0
+elif g >= 5:
+    gs= g*4
+    l=int(input('How tall is your loop in cm:'))
+ls= l*6
+rt = int(input('What is the given run time in seconds:'))
+t= int(input('What is your time:'))
+ts= t*5
+if t>rt:
+    ts= ts*-5
+s= hs+gs+ls+ts
+print(f'Your final score is {s}')
